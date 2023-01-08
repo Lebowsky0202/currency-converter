1c164e99575a75f7c868a1c69a8aa7b7445349db
Kuat <tipoproger@gmail.com>
2023-01-09 01:02:43 +0600

    Convert currency

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const enteredData = document.querySelector('.form__data input');
+const formBtn = document.querySelector('.form__btn');
+const checkboxData = document.querySelectorAll('.form__checkbox');
+const resultBanner = document.querySelector('.result');
+const USD = 400;
+const EUR = 500;
+const GBP = 450;
+
+enteredData.addEventListener('input', () => {
+    enteredData.value = enteredData.value.replace(/\D/, '');
+});
+function countMoney(localСurrency, anotherCurrency){
+    return localСurrency / anotherCurrency;
+}
+
+
+formBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    checkboxData.forEach((item) => {
+        if(item.checked){
+            switch (item.getAttribute('id')) {
+                case 'usd':
+                    resultBanner.textContent = `${enteredData.value} тенге будет ${countMoney(enteredData.value, USD)} долларов США`;
+                    break;
+                case 'eur':
+                    resultBanner.textContent = `${enteredData.value} тенге будет ${countMoney(enteredData.value, EUR)} евро`;
+                    break;
+                case 'gbp':
+                    resultBanner.textContent = `${enteredData.value} тенге будет ${countMoney(enteredData.value, GBP)} фунтов Стерлинга`;
+                    break;
+            }
+        }
+    });
+});
